3465ce5ee2730eaa231e78f773158675a17a7c19
raythurman2386 <raymondthurman5@gmail.com>
2019-10-31 11:39:21 -0400

    basic login form structure

--- auth-friends/src/components/Login.js
+++ auth-friends/src/components/Login.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = props => {
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(credentials);
+  };
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <h2>Login Component</h2>
-    </div>
+      <input
+        name='username'
+        value={credentials.username}
+        onChange={e => handleChange(e)}
+        placeholder='Username'
+      />
+      <input
+        name='password'
+        value={credentials.password}
+        onChange={e => handleChange(e)}
+        placeholder='Password'
+      />
+      <button>Submit</button>
+    </form>
   );
 };
 
