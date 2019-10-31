2d617ee78cda7ac8ab6cd2b1f23aad9f3f33cb58
raythurman2386 <raymondthurman5@gmail.com>
2019-10-31 11:33:37 -0400

    added Login Component

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Login from './components/Login';
 
 function App(props) {
   console.log(props);
   return (
     <div className='App'>
       <h1>Auth Friends</h1>
+      <Login />
     </div>
   );
 }
