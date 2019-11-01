2814d8dfec565a0f0521a274274af419b138c6ae
raythurman2386 <raymondthurman5@gmail.com>
2019-10-31 22:05:12 -0400

    conditionally rendering links

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import { Link, Route, Redirect } from 'react-router-dom';
+
+// components
 import Login from './components/Login';
-import { Link, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+import { getToken } from './utils/axiosConfig';
 
 function App(props) {
+  const loggedIn = getToken();
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    return <Redirect to='/login' />;
+  };
+
   // console.log(props);
+
   return (
     <div className='App'>
       <h1>Auth Friends</h1>
       <nav>
         <Link to='/'>Home</Link>
-        <Link to='/login'>Login</Link>
-        <Link to='/dashboard'>Dashboard</Link>
+        {!loggedIn && <Link to='/login'>Login</Link>}
+        {loggedIn && <Link to='/dashboard'>Dashboard</Link>}
+        {loggedIn && <Link onClick={logout}>Logout</Link>}
       </nav>
       <Route exact path='/login' component={Login} />
+      <PrivateRoute exact path='/dashboard' component={Dashboard} />
     </div>
   );
 }
