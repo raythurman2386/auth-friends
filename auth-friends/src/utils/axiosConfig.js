4f8ad09d080351d49dee8ac6a3f3e1dc119d5d8b
raythurman2386 <raymondthurman5@gmail.com>
2019-10-31 21:53:19 -0400

    axios config corrected

--- auth-friends/src/utils/axiosConfig.js
+++ auth-friends/src/utils/axiosConfig.js
@@ -1,19 +1,14 @@
 import axios from 'axios';
 
-export const axiosWithoutAuth = () => {
-  return axios.create({
-    baseURL: 'http://localhost:5000/api'
-  });
+export const getToken = () => {
+  return localStorage.getItem('token');
 };
 
 export const axiosWithAuth = () => {
-  const token = localStorage.getItem('token');
-
   return axios.create({
     baseURL: 'http://localhost:5000/api',
     headers: {
-      'Content Type': 'application/json',
-      Authorization: token
+      Authorization: localStorage.getItem('token')
     }
   });
 };
