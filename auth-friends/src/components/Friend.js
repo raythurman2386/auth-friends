7112c238bf176c6125f8a7ab91c4b4cb068351f0
raythurman2386 <raymondthurman5@gmail.com>
2019-10-31 22:33:19 -0400

    added edit and delete buttons, MVP Complete

--- auth-friends/src/components/Friend.js
+++ auth-friends/src/components/Friend.js
@@ -6,6 +6,8 @@ const Friend = props => {
       <h3>{props.friend.name}</h3>
       <p>{props.friend.age}</p>
       <p>{props.friend.email}</p>
+      <button>Edit</button>
+      <button>Delete</button>
     </div>
   );
 };
