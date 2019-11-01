a664f036084ed6ec4ec0cd6fdd85d4a21c4a01cf
raythurman2386 <raymondthurman5@gmail.com>
2019-10-31 21:53:51 -0400

    links and login route

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Login from './components/Login';
+import { Link, Route } from 'react-router-dom';
 
 function App(props) {
-  console.log(props);
+  // console.log(props);
   return (
     <div className='App'>
       <h1>Auth Friends</h1>
-      <Login />
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/dashboard'>Dashboard</Link>
+      </nav>
+      <Route exact path='/login' component={Login} />
     </div>
   );
 }
