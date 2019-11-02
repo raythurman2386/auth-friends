329256583495ed8af0cfa8cd5c6ccd1636907b37
raythurman2386 <raymondthurman5@gmail.com>
2019-11-01 22:13:24 -0400

    added navbar component

--- auth-friends/src/App.js
+++ auth-friends/src/App.js
@@ -1,35 +1,17 @@
 import React from 'react';
-import { Link, Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 // components
+import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
 import AddFriend from './components/AddFriend';
-import { getToken } from './utils/axiosConfig';
 
 function App(props) {
-  const loggedIn = getToken();
-
-  const logout = () => {
-    localStorage.removeItem('token');
-    return <Redirect to='/login' />;
-  };
-
   return (
     <div className='App'>
-      <h1>Auth Friends</h1>
-      <nav>
-        <Link to='/'>Home</Link>
-        {!loggedIn && <Link to='/login'>Login</Link>}
-        {loggedIn && <Link to='/dashboard'>Dashboard</Link>}
-        {loggedIn && <Link to='/addfriend'>Add Friend</Link>}
-        {loggedIn && (
-          <Link to='/' onClick={logout}>
-            Logout
-          </Link>
-        )}
-      </nav>
+      <Navbar {...props} />
       <Route exact path='/login' component={Login} />
       <PrivateRoute exact path='/dashboard' component={Dashboard} />
       <PrivateRoute exact path='/addfriend' component={AddFriend} />
